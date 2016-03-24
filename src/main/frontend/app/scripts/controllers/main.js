3024deb70908d98ea5874a48d2c0c3f542338c7a
fathergoose <al@ilseman.com>
2016-03-24 14:17:20 -0500

    shold see a listing on front page now

--- src/main/frontend/app/scripts/controllers/main.js
+++ src/main/frontend/app/scripts/controllers/main.js
@@ -8,10 +8,11 @@
  * Controller of the phonebookApp
  */
 angular.module('phonebookApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', ['listings', function (listings) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    this.listings = listings.query();
+  }]);
