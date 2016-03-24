5159b9d9eca2d48f293d4f9c7e3ceb5c3977345a
fathergoose <al@ilseman.com>
2016-03-24 07:33:58 -0500

    scaffolded using familar tools;

---
+++ src/main/frontend/app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name phonebookApp
+ * @description
+ * # phonebookApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('phonebookApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ src/main/frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phonebookApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the phonebookApp
+ */
+angular.module('phonebookApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
